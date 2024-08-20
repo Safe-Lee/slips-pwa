c5374fecf18f67a5fe707d544a903455307a10ef
Safe-Lee <Daniel.Ferreira@britsafe.org>
2024-08-20 18:39:40 +0100

    Add files via upload

--- checklistLogic.js
+++ checklistLogic.js
@@ -1,54 +1,141 @@
 window.onload = function(){
+	// Each section below initializes inputs for each type (sets existing values, if they exist, and adds eventlisteners, mostly for saving)
+	
+	// INITIALIZE CHECKBOXES
 	var listOfCheckboxes = document.querySelectorAll(`[type*="checkbox"]`);
+	var savedState;
+	var tempIDnumber;
 	listOfCheckboxes.forEach( el => {
-		var checked = JSON.parse(localStorage.getItem(el.id));
-		document.getElementById(el.id).checked = checked;
-		var justIDnumber = el.id.match(/\d+$/);
-		justIDnumber = parseInt(justIDnumber, 10);
-		setSuggestedVisibilityBasedOnCheckbox(el.checked,justIDnumber);
-			
+		// For each checkbox on the page...
+		
+		// Grab the event ID number (useful later)
+		tempIDnumber = grabNumberFromEventID(el);		
+		
+		// Attempt to grab its saved state in localstorage
+		savedState = JSON.parse(localStorage.getItem(el.id));
+		if(savedState!=null){
+			// If there is a saved state for this checkbox...
+			// Set the checkbox on the page to that state
+			document.getElementById(el.id).checked = savedState;
+			// And set the visibility of the Suggested Actions box accordingly
+			setSuggestedVisibilityBasedOnCheckbox(el.checked,tempIDnumber);
+		}else{
+			// If there is no saved state for this checkbox yet...
+			// Assume it's false
+			setSuggestedVisibilityBasedOnCheckbox(false,tempIDnumber);
+		}
+		
+		// Attach an eventlistener to that checkbox, so that every time it's changed...
 		el.addEventListener('change', (event) => {
-			// Update value in localstorage 
-			localStorage.setItem(el.id, el.checked);	
-			//alert("this changed: "+justIDnumber+ " to "+ el.checked);	
-			setSuggestedVisibilityBasedOnCheckbox(el.checked,justIDnumber);
+			//... it updates its value on localstorage
+			localStorage.setItem(el.id, el.checked);
+			//... and sets the Suggested Action visibility accordingly
+			tempIDnumber = grabNumberFromEventID(el);
+			setSuggestedVisibilityBasedOnCheckbox(el.checked,tempIDnumber);
 			console.log(el.id,el.checked);					
 		})
 	});
+	
+	// INITIALIZE TEXT FIELDS	
 	var listOfInputs = document.querySelectorAll(`[type*="text"]`);
+	var savedText;
 	listOfInputs.forEach( el => {
-		var valor = localStorage.getItem(el.id);
-		document.getElementById(el.id).value = valor;
- 
-		el.addEventListener('input', (event) => {
-			localStorage.setItem(el.id, el.value);
-			console.log(el.id,el.value);						
-			//alert('i changed');
+		// For each text field on the page
+
+		// Attempt to grab its saved text in localstorage
+		savedText = localStorage.getItem(el.id);
+		if(savedText!=null){
+			// If there is a saved text for this text box...			
+			// Set the text box to be that text
+			document.getElementById(el.id).value = savedText;
+		}
+
+		// Attach an eventlistener to that text box, so that every time it loses focus..
+		el.addEventListener('blur', (event) => {
+			if(el.value!=null){
+				// It's value is saved to localstorage
+				localStorage.setItem(el.id, el.value);
+				console.log(el.id,el.value);						
+			}
 		})
 	
-	});					
+	});
+
+	// INITIALIZE DATE FIELDS
+	var listOfInputs = document.querySelectorAll(`[type*="date"]`);
+	var savedDate;
+	listOfInputs.forEach( el => {
+		// For each date input on the page (just 1)
+
+		// Attempt to grab the saved date in localstorage
+		savedDate = localStorage.getItem(el.id);
+		if(savedDate!=null){
+			// If there is a saved date...			
+			// Set the date to be that 
+			document.getElementById(el.id).value = savedDate;
+		}
+
+		// Attach an eventlistener to that date input, so that every time it loses focus...
+		el.addEventListener('blur', (event) => {
+			if(el.value!=null){
+				// Its saved to localstorage
+				localStorage.setItem(el.id, el.value);
+				console.log(el.id,el.value);						
+			}
+		})
+	
+	});			
+
+	// INITIALIZE TEXTAREA (at the bottom)
+	var bigTextArea = document.querySelector('#TextAdditional');
+	var savedBigText = localStorage.getItem(bigTextArea.id);
+	if(savedBigText!=null){
+		// If there is a saved date...			
+		// Set the date to be that 
+		bigTextArea.value = savedBigText;
+	}
+	bigTextArea.addEventListener('blur', (event) => {
+		if(bigTextArea.value!=null){
+			// Its saved to localstorage
+			localStorage.setItem(bigTextArea.id, bigTextArea.value);
+			console.log(bigTextArea.id,bigTextArea.value);						
+		}
+	})
+	
 }
 
 save = function(){
-	var list = document.querySelectorAll(`[type*="checkbox"]`);
-	list.forEach( el => {
-		localStorage.setItem(el.id, el.checked);
-		console.log(el.id,el.checked);
-	})
+	// No longer used, as save happens automatically
 }
 			
 clearz = function(){
-  localStorage.clear();
-  window.location.reload();
+	// Clears the localstorage data, and reloads the page (the name "clear" couldn't be used)
+	//if (confirm("⚠️ Are you sure? You will lose all the work you've done.")){
+	if (1){
+		localStorage.clear();
+		window.location.reload();
+	}
 }
 
+// Other useful functions:
 setSuggestedVisibilityBasedOnCheckbox = function(checkStatus,theID){
+	// This changes the visibility of the Suggested Actions box based on the state of its related checkbox 
 	var tempSuggestedAction = document.getElementById("suggestedAction"+theID);
 	if (tempSuggestedAction){ // only check if element exists (otherwise it breaks)
 		if (checkStatus) {
-			tempSuggestedAction.style.display = "inline-block";
-		} else {
-			tempSuggestedAction.style.display = "none";
+			if(tempSuggestedAction.classList.contains("hide")){
+				tempSuggestedAction.classList.remove("hide");
+			}
+		}else{
+			if(!tempSuggestedAction.classList.contains("hide")){
+				tempSuggestedAction.classList.add("hide");
+			}			
 		}
 	}
 }
+grabNumberFromEventID = function(evento){
+	tempIDnumber = evento.id.match(/\d+$/);
+	return parseInt(tempIDnumber, 10);	
+}
+
+// End
